5287ca215b44a21a84c87c05bcad0482c500a526
Adam Knieć <adam.kniec@netguru.com>
2022-04-24 13:59:12 +0200

    test should not deploy

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Deployed with
-          CircleCI
+          Edit <code>src/App.js</code> and save to reload. Deployed with. NIe
+          powinno byc deploya CircleCI
         </p>
         <a
           className="App-link"
